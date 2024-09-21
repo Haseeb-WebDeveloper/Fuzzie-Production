682c3c8f172e56ac747ec4351564cdf5700dd344
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2024-09-21 08:38:09 +0500

    api setup

--- src/app/(main)/(pages)/connections/_components/ConnectionCard.tsx
+++ src/app/(main)/(pages)/connections/_components/ConnectionCard.tsx
@@ -1,80 +1,71 @@
-// Import necessary components and types
-import { Card, CardHeader, CardContent, CardFooter, CardTitle, CardDescription } from '@/components/ui/card';
 import { ConnectionTypes } from '@/lib/types'
-import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react'
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import Image from 'next/image'
+import Link from 'next/link'
 
-// Define the props for the ConnectionCard component
 type Props = {
-    type: ConnectionTypes;
-    icon: string;
-    title: ConnectionTypes;
-    description: string;
-    // callback: () => void; // Commented out, possibly for future use
-    connected: {} & any; // Object to track connection status
+  type: ConnectionTypes
+  icon: string
+  title: ConnectionTypes
+  description: string
+  callback?: () => void
+  connected: {} & any
 }
 
-// ConnectionCard component definition
-function ConnectionCard({
-    type,
-    icon,
-    title,
-    description,
-    // callback, // Commented out, possibly for future use
-    connected,
-}: Props) {
+const ConnectionCard = ({
+  description,
+  type,
+  icon,
+  title,
+  connected,
+}: Props) => {
   return (
-   <>
-      <Card className='flex items-center justify-between w-full'>
-        <CardHeader className="flex flex-col gap-4">
-            {/* Icon container */}
-            <div className='flex flex-row gap-2'>
-                <Image 
-                src={icon} 
-                alt={title} 
-                width={30} 
-                height={30} 
-                className='object-contain'
-                />
-            </div>
-            {/* Title and description */}
-            <div>
-                <CardTitle className='text-lg'>{title}</CardTitle>
-                <CardDescription>{description}</CardDescription>
-            </div>
-        </CardHeader>
-        {/* Connection status or connect button */}
-        <div className='flex flex-col gap-2 items-center p-4'>
-            {connected[type] ? (
-                // Display 'Connected' if the service is connected
-                <div className='border-bg-primary rounded-lg border-2 px-3 py-2 fond-bold text-white'>
-                   Connected
-                </div>
-            )
-            :(
-                // Display 'Connect' button if not connected
-                <Link 
-                    href={
-                        // Determine the correct redirect URL based on the service
-                        title === 'Discord' 
-                        ? process.env.NEXT_PUBLIC_DISCORD_REDIRECT!
-                        : title === 'Notion' 
-                        ? process.env.NEXT_PUBLIC_NOTION_AUTH_URL!
-                        : title === 'Slack' 
-                        ? process.env.NEXT_PUBLIC_SLACK_REDIRECT!
-                        : '#'
-                    }
-                    className='bg-primary p-2 rounded-lg font-bold text-primary-foreground'
-                >
-                    Connect
-                </Link>
-            )}
+    <Card className="flex w-full items-center justify-between">
+      <CardHeader className="flex flex-col gap-4">
+        <div className="flex flex-row gap-2">
+          <Image
+            src={icon}
+            alt={title}
+            height={30}
+            width={30}
+            className="object-contain"
+          />
         </div>
-      </Card>
-   </>
+        <div>
+          <CardTitle className="text-lg">{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </div>
+      </CardHeader>
+      <div className="flex flex-col items-center gap-2 p-4">
+        {connected[type] ? (
+          <div className="border-bg-primary rounded-lg border-2 px-3 py-2 font-bold text-white">
+            Connected
+          </div>
+        ) : (
+          <Link
+            href={
+              title == 'Discord'
+                ? process.env.NEXT_PUBLIC_DISCORD_REDIRECT!
+                : title == 'Notion'
+                ? process.env.NEXT_PUBLIC_NOTION_AUTH_URL!
+                : title == 'Slack'
+                ? process.env.NEXT_PUBLIC_SLACK_REDIRECT!
+                : '#'
+            }
+            className=" rounded-lg bg-primary p-2 font-bold text-primary-foreground"
+          >
+            Connect
+          </Link>
+        )}
+      </div>
+    </Card>
   )
 }
 
-// Export the ConnectionCard component
 export default ConnectionCard
