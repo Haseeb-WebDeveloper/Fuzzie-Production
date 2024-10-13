97d80f875edc37f36b4290dd811117d9adcb1a6d
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2024-10-13 16:08:45 +0500

    final push

--- src/app/(main)/(pages)/workflows/_components/index.tsx
+++ src/app/(main)/(pages)/workflows/_components/index.tsx
@@ -12,16 +12,22 @@ const index = async (props: Props) => {
     // this is the section that contain the div that contains the workflows
         <section className=' w-full mt-2'>
          
-          <div className='columns-1 md:columns-2 lg:columns-3 p-5 space-y-4 gap-5  '>
+          <div className=' '>
           {workflows?.length
-           ? workflows.map((flow, index)=>(
-            <div className='shadow-md break-inside-avoid' key={index}> 
-            <WorkflowCard
-              {...flow}
-              />
-              </div>
-            )) : (
-              <div className='mx-auto text-center w-full mt-20'>
+           ? (
+           <div className='columns-1 md:columns-2 lg:columns-3 p-5 space-y-4 gap-5  '>
+            {
+               workflows.map((flow, index)=>(
+                <div className='shadow-md break-inside-avoid' key={index}> 
+                <WorkflowCard
+                  {...flow}
+                  />
+                  </div>
+            ) )
+            }
+           </div> )
+             : (
+              <div className='mx-auto text-center w-full mt-20 '>
                 <p className=' text-lg text-muted-foreground'>No workflow found 🔍</p>
               </div>
             )
